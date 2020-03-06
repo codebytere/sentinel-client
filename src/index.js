65b3d73c5b837fbbb576dd67c544d8ae40539222
Shelley Vohr <shelley.vohr@gmail.com>
2020-03-06 10:30:07 -0800

    fix: send more real data

--- src/index.js
+++ src/index.js
@@ -28,34 +28,30 @@ async function run() {
   try {
     const clientPayload = github.context.payload.client_payload
 
-    core.debug(`clientPayload: ${inspect(clientPayload)}`)
     const { platformInstallData, reportCallback, sessionToken, name } = clientPayload
 
     const reportPath = path.resolve('report', 'report.json')
     const rawData = await fs.readFile(reportPath, 'utf8')
     const report = JSON.parse(rawData)
-    core.debug(`report: ${inspect(report)}`);
 
     const sysData = platformInstallData.platform.split('-')
-    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-    const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
-    const statuses = ['Passed', 'Failed', 'Skipped']
+    const lastTest = report.testResults[report.testResults.length - 1]
 
     const testData = {
-      name: `${platformInstallData.platform}-${Date.now()}`,
-      status: statuses[Math.floor(Math.random() * 3)],
+      name: `${name}-${platformInstallData.platform}-${Date.now()}`,
+      status: report.status ? 'Passed' : 'Failed',
       os: sysData[0],
       arch: sysData[1],
-      sourceLink: `https://www.${name}.com`,
-      timeStart: date,
-      timeStop: date,
-      totalPassed: getRandomInt(25),
-      totalSkipped: getRandomInt(25),
-      totalWarnings: getRandomInt(25),
-      totalFailed: getRandomInt(25),
-      workspaceGzipLink: `https://www.${name}.com`,
-      logfileLink: `https://www.${name}.com`,
-      ciLink: `https://www.${name}.com`,
+      sourceLink: 'https://github.com/electron/fiddle',
+      timeStart: new Date(report.timeStart),
+      timeStop: new Date(lastTest.endTime),
+      totalPassed: report.numPassedTests,
+      totalSkipped: report.numTodoTests,
+      totalWarnings: 0,
+      totalFailed: report.numFailedTests,
+      workspaceGzipLink: 'N/A',
+      logfileLink: 'N/A',
+      ciLink: 'N/A',
       testAgent: testAgent()
     }
 
